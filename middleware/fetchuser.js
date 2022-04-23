197fecfb3ef8f4ba5a6c5a01f988a2ff0b5959d0
Diptabose <83805205+Diptabose@users.noreply.github.com>
2022-04-23 14:58:53 +0530

    Add files via upload

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -1,1 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const fetchuser= (req,res,next)=>{
+  const token = req.header('auth-token');
+  if(!token){
+    res.status(401).json({msg:'Cant fetch user details'});
+  }
+  try{
+    const data = jwt.verify(token,'12511255');
+   // console.log(data);
+    req.id= data.id;
+  }
+  catch(error)
+  {
+    res.status(401).json({msg:'Cant fetch user details'});
+  }
+  next();
+};
+
+module.exports={fetchuser};
 
