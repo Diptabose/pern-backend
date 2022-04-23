0a372e5186d4e22aa4399ded5baf35c99e716619
Diptabose <83805205+Diptabose@users.noreply.github.com>
2022-04-23 14:56:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+const cors = require('cors');
 const express = require('express');
 const {pool}= require('./dbConnect');
 
 const app= express();
 const PORT= process.env.PORT||5000;
 
+//Allows cross origin requests..
+app.use(cors({
+  origin: '*'
+}));
 //Allows to get body in the request
 app.use(express.json());
+
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'));
