de86d87aefbdc0f23ed7aeb301d24001b8db7259
Diptabose <83805205+Diptabose@users.noreply.github.com>
2022-04-23 14:58:08 +0530

    Add files via upload

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,15 @@ const express = require('express');
 //Npm package to validate user details...
 const { body, validationResult } = require('express-validator');
 const router = express.Router();
-const {registerUser} = require('../schemas/register');
+const {registerUser,isUserLogged,getuserDetails} = require('../schemas/register');
 //npm package for authorization of valid users
 const jwt = require('jsonwebtoken');
 
+const {fetchuser} = require('../middleware/fetchuser');
+
+
+
+//Route 1 to create a user from the credentials (name , email, password) passed , No login required . Also returns a JWT token . POST request
 router.post('/createuser',
 body('name','Invalid name').isLength({ min: 5 }),
 body('password','Invalid password').isLength({ min: 8 }),
@@ -16,15 +21,57 @@ async (req,res)=>{
       return res.status(400).json({ errors: errors.array() });
     }
     try{
-      var authToken = jwt.sign({name:req.body.name}, '12511255');
       const resp= await registerUser(req.body);
+      var authToken = jwt.sign({id:resp.id}, '12511255');
       res.json(authToken);
-      
     }
     catch(error){
       const {msg, code, status} = error;
-       res.status(400).json({status,msg,code});
+       res.status(400).json({msg,code ,error});
     }
-    
 });
+
+
+//Route 2 Logging in an user with correct credentials (email and password) , also returning a JWT Token to user. POST request
+router.post('/login',
+body('password','Invalid password').isLength({ min: 8 }),
+body('email','Invalid Email').isEmail(),
+async (req,res)=>{
+  const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  try{
+    const resp= await isUserLogged(req.body);
+    if(resp.status===200){
+     // console.log('ID at login '+ resp.id);
+    var authToken = jwt.sign({id:resp.id}, '12511255');
+    //console.log('JWT TOKEN AT LOGIN '+authToken);
+    res.json(authToken);
+    }
+    else{
+      return res.status(resp.status).json({msg:resp.msg});
+    }
+  }
+  catch(error)
+  {
+    console.log(error);
+    return res.status(400).json({msg:error.msg});
+  }
+});
+
+
+//Route 3 Fetching user details , with the JWT Token passed into the header of the request as auth-token.POST request
+router.post('/getuser',fetchuser,async (req,res)=>{
+     try{
+       
+       const userDetails = await getuserDetails(req.id);
+       const {id, name , email,date} = userDetails;
+       return res.status(200).json({id,name,email,date});
+     }
+     catch(error){
+       res.status(400).json({msg:error.msg});
+     }
+});
+
 module.exports=router;
--- routes/notes.js
+++ routes/notes.js
@@ -1,14 +1,81 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-  
-  let obj= {
-    name:'Bose',
-    email:'diptabose484@gmail.com',
+//Middleware to deckde the username from the jwt token
+const {fetchuser} = require('../middleware/fetchuser');
+//SQL QUERY functions are imported here
+const {fetchNotes,addNote,updateNotes,findNoteId,deleteNotes} = require('../schemas/noteoperations');
+
+//Route 1 to fetch all notes of the user using the auth token semt in the header . Its a get request . Returns all notes of that user. Login required 
+router.get('/fetchallnotes',fetchuser,async(req,res)=>{
+  try{
+    const notes = await fetchNotes(req.id);
+    res.json({notes});
   }
-  res.json(obj);
+  catch(error){
+    res.status(400).json({msg:error.msg});
+  }
 })
 
-module.exports=router;
 
+//Route 2 Add a note to the database . Using POST request details (TITLE , TAG , DESCRIPTION ) are sent in body as well as auth token in header . Returns whether there was a succesfull insertion or not .
+
+router.post('/addnote',fetchuser,async(req,res)=>{
+    let note={id: req.id, ...req.body}
+    try{
+      const added = await addNote(note);
+      res.status(added.status).json({added});
+     }
+    catch(error){
+        res.status(error.status).json({msg:error.msg});
+    }
+});
+
+
+//Route 3 Updating a note of the specified user. Using POST request sending update details ( title , tag, description) in request body and auth token in the req header. Uses dynamic routing to send note id
+
+router.patch('/updatenote/:id',fetchuser,async (req,res)=>{
+  try{
+  const didNoteIdFound = await findNoteId(req.params.id);
+  const {found , id , userid} = didNoteIdFound;
+  if(!found){
+    return res.status(404).json({msg:'Note not found'});
+  }
+  if(!id || userid!==req.id){
+    return res.status(401).json({msg:'Unauthorized user'});
+  }
+  const resp= await updateNotes(req.body);
+  res.status(200).json({msg:'Note updated'});
+  }
+  catch(error){
+    //console.log(error);
+    res.status(error.status).json({msg:error.msg});
+  }
+});
+
+//Route 4 Deleting a note of the specified user. Using DELETE request ,sending the auth token in the req header. Uses dynamic routing to send noteid.
+
+router.delete('/deletenote/:id',fetchuser,async(req,res)=>{
+  try{
+    const didNoteIdFound = await findNoteId(req.params.id);
+  const {found , id , userid} = didNoteIdFound;
+  if(!found){
+    return res.status(404).json({msg:'Note not found'});
+  }
+  console.log(found);
+  console.log(id);
+  console.log(userid);
+  console.log(req.id);
+  if(!id || userid!==req.id){
+    return res.status(401).json({msg:'Unauthorized user'});
+  } 
+  const resp= await deleteNotes(req.params.id);
+  res.status(200).json({msg:resp.msg});
+  }
+  catch(error)
+  {
+    res.status(error.status).json({msg:error.msg});
+  }
+});
+
+module.exports=router;
